24c63c592bba1298478268b47c7587fd5f913680
Gergely Csucs <Tevemadar@users.noreply.github.com>
2023-09-27 15:48:22 +0200

    Headless mode
    11 arguments:
    - JSON
    - 9 coordinates
    - destination folder

--- qnlmask/QuickMask.java
+++ qnlmask/QuickMask.java
@@ -34,6 +34,15 @@ public class QuickMask {
 	public static boolean scattering = false;
 
 	public static void main(String[] args) /* throws Exception */ {
+		if (args.length == 11) {
+			try {
+				generateMasks(args);
+			} catch (Exception ex) {
+				System.err.println(ex);
+				System.exit(1);
+			}
+			return;
+		}
 		final JFrame frame = new JFrame("QuickMask Emergency UI");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLayout(new GridBagLayout());
@@ -191,79 +200,9 @@ public class QuickMask {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				try {
-					final double cx = Double.parseDouble(corn[0].getText());
-					final double cy = Double.parseDouble(corn[1].getText());
-					final double cz = Double.parseDouble(corn[2].getText());
-					final double nux = Double.parseDouble(corn[3].getText()) - cx;
-					final double nuy = Double.parseDouble(corn[4].getText()) - cy;
-					final double nuz = Double.parseDouble(corn[5].getText()) - cz;
-					final double nvx = Double.parseDouble(corn[6].getText()) - cx;
-					final double nvy = Double.parseDouble(corn[7].getText()) - cy;
-					final double nvz = Double.parseDouble(corn[8].getText()) - cz;
-					final double nx = nuy * nvz - nvy * nuz;
-					final double ny = nuz * nvx - nvz * nux;
-					final double nz = nux * nvy - nvx * nuy;
-
-					File f = new File(file.getText());
-					Series series = new Series();
-					try (FileReader fr = new FileReader(f)) {
-						Map<String, String> resolver = new HashMap<>();
-						resolver.put("resolution", "target-resolution");
-						JSON.mapObject(JSON.parse(fr), series, resolver);
-					} catch (Exception ex) {
-					}
-					if (series.slices.size() < 1) {
-						JOptionPane.showMessageDialog(null, "Error", f.getName() + " is not compatible with VisuAlign.",
-								JOptionPane.ERROR_MESSAGE);
-						return;
-					}
-					series.propagate();
-
-					for (Slice slice : series.slices) {
-						double ox = slice.anchoring.get(0);
-						double oy = slice.anchoring.get(1);
-						double oz = slice.anchoring.get(2);
-						double ux = slice.anchoring.get(3);
-						double uy = slice.anchoring.get(4);
-						double uz = slice.anchoring.get(5);
-						double vx = slice.anchoring.get(6);
-						double vy = slice.anchoring.get(7);
-						double vz = slice.anchoring.get(8);
-						int width = (int) Math.sqrt(ux * ux + uy * uy + uz * uz) + 1;
-						int height = (int) Math.sqrt(vx * vx + vy * vy + vz * vz) + 1;
-						byte mask[][] = new byte[height*3][width*3];
-						for (int v = -height; v < height*2; v++)
-							for (int u = -width; u < width*2; u++)
-								mask[v+height][u+width] = (ox + ux * u / width + vx * v / height - cx) * nx
-										+ (oy + uy * u / width + vy * v / height - cy) * ny
-										+ (oz + uz * u / width + vz * v / height - cz) * nz > 0 ? (byte) 255 : 0;
-						FileOutputStream fos = new FileOutputStream(dir.getText() + File.separator
-								+ slice.filename.substring(0, slice.filename.lastIndexOf('.')) + "_mask.png");
-						PngWriter png = new PngWriter(fos, width, height, PngWriter.TYPE_GRAYSCALE, null);
-
-						slice.triangulate();
-						List<Triangle> triangles = slice.triangles;
-
-						for (int v = 0; v < height; v++) {
-							byte line[] = new byte[width];
-							for (int u = 0; u < width; u++) {
-								double fx = u * slice.width / width;
-								double fy = v * slice.height / height;
-								for (Triangle triangle : triangles) {
-									double t[] = triangle.transform(fx, fy);
-									if (t != null) {
-										int xx = (int) (t[0] * width / slice.width);
-										int yy = (int) (t[1] * height / slice.height);
-										if (xx >= -width && yy >= -height && xx < width*2 && yy < height*2)
-											line[u] = mask[yy+height][xx+width];
-										break;
-									}
-								}
-							}
-							png.writeline(line);
-						}
-						fos.close();
-					}
+					generateMasks(new String[] { file.getText(), corn[0].getText(), corn[1].getText(),
+							corn[2].getText(), corn[3].getText(), corn[4].getText(), corn[5].getText(),
+							corn[6].getText(), corn[7].getText(), corn[8].getText(), dir.getText() });
 					JOptionPane.showMessageDialog(null, "Everything went fine", "All good",
 							JOptionPane.INFORMATION_MESSAGE);
 					frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
@@ -273,5 +212,78 @@ public class QuickMask {
 				}
 			}
 		});
+	}
+
+	public static void generateMasks(String args[]) throws Exception {
+		final double cx = Double.parseDouble(args[1]);
+		final double cy = Double.parseDouble(args[2]);
+		final double cz = Double.parseDouble(args[3]);
+		final double nux = Double.parseDouble(args[4]) - cx;
+		final double nuy = Double.parseDouble(args[5]) - cy;
+		final double nuz = Double.parseDouble(args[6]) - cz;
+		final double nvx = Double.parseDouble(args[7]) - cx;
+		final double nvy = Double.parseDouble(args[8]) - cy;
+		final double nvz = Double.parseDouble(args[9]) - cz;
+		final double nx = nuy * nvz - nvy * nuz;
+		final double ny = nuz * nvx - nvz * nux;
+		final double nz = nux * nvy - nvx * nuy;
+
+		File f = new File(args[0]);
+		Series series = new Series();
+		try (FileReader fr = new FileReader(f)) {
+			Map<String, String> resolver = new HashMap<>();
+			resolver.put("resolution", "target-resolution");
+			JSON.mapObject(JSON.parse(fr), series, resolver);
+		}
+		if (series.slices.size() < 1) {
+			throw new Exception(f.getName() + " is not compatible with VisuAlign.");
+		}
+		series.propagate();
+
+		for (Slice slice : series.slices) {
+			double ox = slice.anchoring.get(0);
+			double oy = slice.anchoring.get(1);
+			double oz = slice.anchoring.get(2);
+			double ux = slice.anchoring.get(3);
+			double uy = slice.anchoring.get(4);
+			double uz = slice.anchoring.get(5);
+			double vx = slice.anchoring.get(6);
+			double vy = slice.anchoring.get(7);
+			double vz = slice.anchoring.get(8);
+			int width = (int) Math.sqrt(ux * ux + uy * uy + uz * uz) + 1;
+			int height = (int) Math.sqrt(vx * vx + vy * vy + vz * vz) + 1;
+			byte mask[][] = new byte[height * 3][width * 3];
+			for (int v = -height; v < height * 2; v++)
+				for (int u = -width; u < width * 2; u++)
+					mask[v + height][u + width] = (ox + ux * u / width + vx * v / height - cx) * nx
+							+ (oy + uy * u / width + vy * v / height - cy) * ny
+							+ (oz + uz * u / width + vz * v / height - cz) * nz > 0 ? (byte) 255 : 0;
+			FileOutputStream fos = new FileOutputStream(args[10] + File.separator
+					+ slice.filename.substring(0, slice.filename.lastIndexOf('.')) + "_mask.png");
+			PngWriter png = new PngWriter(fos, width, height, PngWriter.TYPE_GRAYSCALE, null);
+
+			slice.triangulate();
+			List<Triangle> triangles = slice.triangles;
+
+			for (int v = 0; v < height; v++) {
+				byte line[] = new byte[width];
+				for (int u = 0; u < width; u++) {
+					double fx = u * slice.width / width;
+					double fy = v * slice.height / height;
+					for (Triangle triangle : triangles) {
+						double t[] = triangle.transform(fx, fy);
+						if (t != null) {
+							int xx = (int) (t[0] * width / slice.width);
+							int yy = (int) (t[1] * height / slice.height);
+							if (xx >= -width && yy >= -height && xx < width * 2 && yy < height * 2)
+								line[u] = mask[yy + height][xx + width];
+							break;
+						}
+					}
+				}
+				png.writeline(line);
+			}
+			fos.close();
+		}
 	}
 }
